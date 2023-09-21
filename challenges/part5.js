2c091d6655d60f561bcea0b74613262fafe5136c
jarrodmjack <jarrodmjack@gmail.com>
2023-09-21 19:37:45 -0400

    part 5.1-5.8

--- challenges/part5.js
+++ challenges/part5.js
@@ -1,40 +1,47 @@
-let myName = "";
-let vDOM;
-let elems;
+let vDOM
+let elems
+let data = { myName: "", yourName: "" }
+let isFocus
 
 function createVDOM() {
-  return [
-    [
-      "input",
-      myName,
-      function handle(e) {
-        myName = e.target.value;
-      },
-    ],
-    ["div", `Hello, ${myName}!`],
-    ["div", `Great job, Jonathan!`],
-    ["div", `Great job, Alexa!`],
-    ["div", `Great job, Emilia!`],
-  ];
+	return [
+		[
+			"input",
+			data.myName,
+			function handle(e) {
+				console.log()
+				updateData("myName", e.target.value)
+			},
+		],
+		["div", `Hello, ${data.myName}!`],
+		["div", `Great job, Jonathan!`],
+		["div", `Great job, Alexa!`],
+		["div", `Great job, Emilia!`],
+	]
 }
 
-function updateDOM() {
-  if (vDOM)
-    document.activeElement == document.querySelector("input")
-      ? (isFocus = true)
-      : (isFocus = false); // keep this code
-  vDOM = createVDOM();
-  elems = vDOM.map(convert);
-  document.body.replaceChildren(...elems);
-  if (isFocus) elems[0].focus(); //keep this code
+function updateData(label, value) {
+	data[label] = value
+	window.requestAnimationFrame(updateDOM)
+}
+
+function updateDOM(s) {
+	if (vDOM)
+		document.activeElement == document.querySelector("input")
+			? (isFocus = true)
+			: (isFocus = false) // keep this code
+	vDOM = createVDOM()
+	elems = vDOM.map(convert)
+	document.body.replaceChildren(...elems)
+	if (isFocus) elems[0].focus() //keep this code
 }
 
 function convert(node) {
-  const element = document.createElement(node[0]);
-  element.textContent = node[1];
-  element.value = node[1];
-  element.oninput = node[2];
-  return element;
+	const element = document.createElement(node[0])
+	element.textContent = node[1]
+	element.value = node[1]
+	element.oninput = node[2]
+	return element
 }
 
 // function findDiff(prevVDOM, currentVDOM) {
@@ -45,4 +52,5 @@ function convert(node) {
 //     }
 // }
 
-setInterval(updateDOM, 15);
+window.requestAnimationFrame(updateDOM)
+// setInterval(updateDOM, 15);
