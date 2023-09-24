4eb7b9d6e0eb0b3a218b95aa4b5d94d9c1942781
jarrodmjack <jarrodmjack@gmail.com>
2023-09-23 22:03:51 -0400

    expand on challenge 4

--- challenges/part4.js
+++ challenges/part4.js
@@ -29,22 +29,33 @@ let jsInput
 let jsDiv
 let vDOM
 
-function createVDOM() {
-	return [
-		[
-			"input",
-			myName,
-			function handle() {
-				myName = jsInput.value
-			},
-		],
-		["div", `Hello, ${myName}!`, , ['div', 'I am nested once', , ['div', 'I am nested twice']]],
-	]
+// function createVDOM() {
+// 	return [
+// 		[
+// 			"input",
+// 			myName,
+// 			function handle() {
+// 				myName = jsInput.value
+// 			},
+// 		],
+// 		[
+// 			"div",
+// 			`Hello, ${myName}!`,
+// 			,
+// 			["div", "I am nested once", , ["div", "I am nested twice"]],
+// 		],
+// 	]
+// }
+
+const textsToDisplay = ['Hello from div 1', 'Hello from div 2', 'Hello from div 3']
+
+function CreateDivWithText(text) {
+	return ["div", text]
 }
 
 function updateDOM() {
 	document.activeElement === jsInput ? (isFocus = true) : (isFocus = false)
-	vDOM = createVDOM()
+	vDOM = textsToDisplay.map(CreateDivWithText)
 	let elems = vDOM.map(convert)
 	jsInput = elems[0] //the reason this is needed is because we cannot call focus until after it has been replaced, but we need the link back to the original node
 	document.body.replaceChildren(...elems)
