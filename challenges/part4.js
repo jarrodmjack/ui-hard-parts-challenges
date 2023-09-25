162c0e63e497a90d057c50d6150c8e4fcdf75311
jarrodmjack <jarrodmjack@gmail.com>
2023-09-24 22:00:33 -0400

    challenge 4

--- challenges/part4.js
+++ challenges/part4.js
@@ -1,62 +1,68 @@
-let myName = "";
+// let myName = "";
 
-const vDOM = [
-  [
-    "input",
-    myName,
-    function handle() {
-      myName = jsInput.value;
-    },
-  ],
-  ["div", `Hello, ${myName}!`],
-];
+// const vDOM = [
+//   [
+//     "input",
+//     myName,
+//     function handle() {
+//       myName = jsInput.value;
+//     },
+//   ],
+//   ["div", `Hello, ${myName}!`],
+// ];
 
-function convert(node) {
-  const element = document.createElement(node[0]);
-  element.textContent = node[1];
-  element.value = node[1];
-  element.oninput = node[2];
-  return element;
-}
+// function convert(node) {
+//   const element = document.createElement(node[0]);
+//   element.textContent = node[1];
+//   element.value = node[1];
+//   element.oninput = node[2];
+//   return element;
+// }
 
 /** Step @todo */
 /* uncomment the code below this line, and comment out the code above*/
 
-// let isFocus = false
+let isFocus = false
 
-// let myName = '';
-// let jsInput;
-// let jsDiv;
-// let vDOM;
+let myName = ""
+let jsInput
+let jsDiv
+let vDOM
 
-// function createVDOM() {
-//   return [
-//     [
-//       'input',
-//       myName,
-//       function handle() {
-//         myName = jsInput.value;
-//       },
-//     ],
-//     ['div', `Hello, ${myName}!`],
-//   ];
-// }
+function createVDOM() {
+	return [
+		[
+			"input",
+			myName,
+			function handle() {
+				myName = jsInput.value
+			},
+		],
+		["div", `Hello, ${myName}!`, , ['div', 'I am nested once', , ['div', 'I am nested twice']]],
+	]
+}
 
-// function updateDOM() {
-//   document.activeElement === jsInput ? (isFocus = true) : (isFocus = false);
-//   vDOM = createVDOM();
-//   jsInput = convert(vDOM[0]);
-//   jsDiv = convert(vDOM[1]);
-//   document.body.replaceChildren(jsInput, jsDiv);
-//   if(isFocus) jsInput.focus()
-// }
+function updateDOM() {
+	document.activeElement === jsInput ? (isFocus = true) : (isFocus = false)
+	vDOM = createVDOM()
+	let elems = vDOM.map(convert)
+	jsInput = elems[0] //the reason this is needed is because we cannot call focus until after it has been replaced, but we need the link back to the original node
+	document.body.replaceChildren(...elems)
+	if (isFocus) jsInput.focus()
+}
 
-// function convert(node) {
-//     const element = document.createElement(node[0]);
-//     element.textContent = node[1];
-//     element.value = node[1];
-//     element.oninput = node[2];
-//     return element;
-//   }
+function convert(node) {
+	const element = document.createElement(node[0])
+	element.textContent = node[1]
+	element.value = node[1]
+	element.oninput = node[2]
 
-// setInterval(updateDOM, 15);
+	if (node[3] && Array.isArray(node[3])) {
+		let nestedElem = convert(node[3])
+		element.appendChild(nestedElem)
+	}
+
+	return element
+}
+
+setInterval(updateDOM, 3500)
